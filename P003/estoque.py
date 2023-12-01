34b8bf2bb83f8d388c50047f1b7f1928d8bbd0cb
Diogo Dorea <diogodorea@gmail.com>
2023-12-01 01:25:35 -0300

    estoque excluir

--- P003/estoque.py
+++ P003/estoque.py
@@ -7,3 +7,13 @@ def inserir_produto(lista_produtos):
     lista_produtos.append(produto)
     print("Produto inserido com sucesso!")
 
+def excluir_produto(lista_produtos):
+    codigo = input("Digite o código do produto que deseja excluir: ")
+    produtos_encontrados = [produto for produto in lista_produtos if produto['codigo'] == codigo]
+    
+    if produtos_encontrados:
+        lista_produtos.remove(produtos_encontrados[0])
+        print("Produto removido com sucesso!")
+    else:
+        print("Produto não encontrado.")
+
