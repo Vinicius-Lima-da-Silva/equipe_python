3e63b092bf8347c990b134d10432bfd845725023
Diogo Dorea <diogodorea@gmail.com>
2023-12-04 02:57:17 -0300

    ListaEmOrdem de Nomes

--- PI-004/Parte1/DataFruta.py
+++ PI-004/Parte1/DataFruta.py
@@ -153,6 +153,12 @@ class ListaNomes(AnaliseDados):
             print(f"Alfabeticamente o maior é : {listaTemporaria[tamanhoDaLista-1]}")
         else:
             print("Lista está vazia!")
+      
+    def listarEmOrdem(self):
+        listaOrdenada = sorted(self.__lista)
+        print("Lista em ordem:")
+        for nome in listaOrdenada:
+            print(nome)
 
     def __str__(self):
         for i in self.__lista:
